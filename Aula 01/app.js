3a7a0443deb03bde484b10e4d70c46c45f814745
Lucas Mendonça <lucas21rio@gmail.com>
2021-08-10 16:05:12 -0300

    exercício 01

---
+++ Aula 01/app.js
@@ -0,0 +1,25 @@
+let notas = [3, 8, 7, 6];
+
+let soma = notas.reduce((a, b) => {
+    let sum = a + b;
+        console.log(`Soma: ${sum}`);
+      return sum;
+})
+
+let media = soma/notas.length
+
+console.log(`A média é: ${media}`)
+
+notas.forEach(e => {
+    if(e>7) {
+        console.log(`Parabéns pela conclusão do ${notas.indexOf(e)+1}º bimestre. Sua nota foi ${e}`)
+    } else {
+       console.log(`Infelizmente sua nota do ${notas.indexOf(e)+1}º bimestre foi ${e}`)
+    }
+})
+
+if(media > 7) {
+    console.log(`Você foi aprovado!!! Parabéns!`)
+}else {
+    console.log(`Você foi reprovado.`)
+}
